accb6c2e44a0262bbf80e2cb9f41224f30024f7a
Duncan Temple Lang <duncan@r-project.org>
2017-07-27 05:42:15 -0700

    compiles

--- src/arrow123.cc
+++ src/arrow123.cc
@@ -16,34 +16,42 @@
 #include <Rinternals.h>
 
 
+using namespace arrow;
+
+const int32_t* arrow123;
+
+void init()
+{    
 // Calling the class constructor for Int32Builder
-arrow::Int32Builder builder(arrow::default_memory_pool(), arrow::int32());
+    arrow::Int32Builder builder(arrow::default_memory_pool(), arrow::int32());
 
-builder.Append(1);
-builder.Append(2);
-builder.Append(3);
+    builder.Append(1);
+    builder.Append(2);
+    builder.Append(3);
 
-std::shared_ptr<Array> array;
-builder.Finish(&array);
+    std::shared_ptr<arrow::Array> array;
+    builder.Finish(&array);
 
-// Cast the Array to its actual type to access its data
-std::shared_ptr<Int32Array> int32_array = std::static_pointer_cast<Int32Array>(array);
+        // Cast the Array to its actual type to access its data
+    std::shared_ptr<Int32Array> int32_array = std::static_pointer_cast<Int32Array>(array);
 
-// Get the pointer to the actual data
-const int32_t* arrow123 = int32_array->raw_data();
+      // Get the pointer to the actual data
+    arrow123 = int32_array->raw_values();
+}
 
 
-
-extern "C" {
+extern "C" 
 SEXP c_get123()
 {
     int n = 3;
+
+    init();
 
     // Create the new return object from within C
     SEXP out = PROTECT(allocVector(INTSXP, n));
 
     // C pointer to the actual data
-    int *outp = INT(out);
+    int *outp = INTEGER(out);
 
     // Using C std library to copy memory, Arrow may provide better way
     memcpy(outp, arrow123, n * sizeof(int));
@@ -51,4 +59,4 @@ SEXP c_get123()
     UNPROTECT(1);
     return out;
 }
-}
+
